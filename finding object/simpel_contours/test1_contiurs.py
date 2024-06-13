07672281d2a6bf9d158565ae01ba530bb47bc561
SW Robot <168210737+swrobotgit@users.noreply.github.com>
2024-06-13 14:16:15 +0700

    Update test1_contiurs.py

--- finding object/simpel_contours/test1_contiurs.py
+++ finding object/simpel_contours/test1_contiurs.py
@@ -16,15 +16,58 @@ gray = cv2.Canny(gray, 30, 200)
 # Находим контуры на изображении
 contours, _ = cv2.findContours(gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
+# Подсчитываем количество контуров
+num_contours = len(contours)
+# print(contours)
+# Выводим количество контуров в консоль
+print("Найдено контуров: "+ str(num_contours))
+
+# Перебираем все контуры
+for contour in contours:
+    # Получаем координаты контура
+    for point in contour:
+        x, y = point[0]
+        # Выводим координаты в консоль
+        print(f"Координаты точки: x={x}, y={y}")
+
+# Инициализируем переменные для хранения крайних координат
+top_left = None
+bottom_right = None
+
+# Перебираем все контуры
+for contour in contours:
+    for point in contour:
+        x, y = point[0]
+        # Находим крайние координаты
+        if top_left is None or (x <= top_left[0] and y <= top_left[1]):
+            top_left = (x, y)
+        if bottom_right is None or (x >= bottom_right[0] and y >= bottom_right[1]):
+            bottom_right = (x, y)
+
+# Выводим крайние координаты
+print("Крайняя верхняя левая координата:", top_left)
+print("Крайняя нижняя правая координата:", bottom_right)
 
 
 # Рисуем контуры на изображении
 image_with_contours = image.copy()
-cv2.drawContours(image_with_contours, contours, -1, (0, 255, 0), 3)
+
+# Рисуем прямоугольник на изображении
+cv2.rectangle(image_with_contours, top_left, bottom_right, (0, 0, 0), thickness=2)
+
+# Находим координаты середины прямоугольника
+center_x = (top_left[0] + bottom_right[0]) // 2
+center_y = (top_left[1] + bottom_right[1]) // 2
+
+# Получаем цвет пикселя в середине прямоугольника
+pixel_color = image[center_y, center_x]
+
+# Выводим значение RGB цвета пикселя
+print("RGB значение цвета пикселя в середине прямоугольника:"+ str(pixel_color))
 
 # Добавляем текст "Это был Красный квадрат" в левом верхнем углу
 font = cv2.FONT_HERSHEY_COMPLEX
-cv2.putText(image_with_contours, "Это был Красный квадрат", (50, 50), font, 1, (255, 0, 0), 2)
+cv2.putText(image_with_contours, "Цвет этого прямоугольника: "+str(pixel_color), (50, 50), font, 1, (255, 0, 0), 1)
 
 # Сохраняем изображение с контурами и текстом
 cv2.imwrite('test_rectangle_find.jpg', image_with_contours)
